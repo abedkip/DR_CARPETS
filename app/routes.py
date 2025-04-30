25699d32ef7c9e1ee7d3143369dfdc852f07c522
Kipngeno Abednego <abnasgit@gmail.com>
2025-04-30 16:11:37 +0300

    added other urls

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('homepage.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
