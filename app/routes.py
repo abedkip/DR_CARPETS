b478d10fac1ab90d8727b01a072f8b40a08f4c6c
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-01 10:34:47 +0300

    added dr deals url

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,35 @@ def home():
 @app.route('/test')
 def test():
     return render_template('test.html')
+
+
+@app.route('/deals')
+def deals():
+    products = [
+        {
+            "name": "Alin Abstract Carpet",
+            "image": "Alin Abstract Carpet",
+            "old_price": "11,929.00",
+            "name": "8,049.00",
+            "sold_out": True,
+            "url": "#"
+        },
+        {
+              "name": "Alin Beige Dream Carpet",
+            "image": "",
+            "old_price": "11,929.00",
+            "name": "8,049.00",
+            "sold_out": True,
+            "url": "#"
+        }
+        
+        
+        
+        
+        
+        
+    ]
+    
+    colors = ["A.Bej/A.Bej", "A.Gri/A.Gri", "A.Gri/A.Mavi", "A.Gri/Beyaz"]
+    
+    return render_template("dr_deals.html", products=products, colors=colors)
