764c268326810e29c0b5852265b96c0176a9be99
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-16 11:22:06 +0300

    add sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask (__name__)
+app.config['SECRET_KEY']='837ff881ff74c5fc48e9a3e5b177960ca1db7488c6edf91a9b38f9f29250e5a0'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///drcarpets.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
+db = SQLAlchemy(app)
 
 from app import routes
+from app import models
+
+
