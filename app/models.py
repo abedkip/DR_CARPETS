013ab3a9da25b076d24ce9d140c812b2cee935bd
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-16 11:25:42 +0300

    basal commit

---
+++ app/models.py
@@ -0,0 +1,30 @@
+
+from app import db, app
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    cart_items = db.relationship('CartItem', backref='user', lazy=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, default=1)
+    product = db.relationship('Product')
+
+
+
+
+  
+with app.app_context():
+    db.create_all()
+ 
