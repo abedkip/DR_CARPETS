91c9dea54df9671f91c77dbb2388b4dcac07074c
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-17 01:46:30 +0300

    add submitfield

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Length, email
+from wtforms.validators import DataRequired, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), email()])
+    email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(),
+        EqualTo('password', message='Passwords must match')
+    ])
+    submit = SubmitField('Register')
