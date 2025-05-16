2e21735be1be595b7b70acf6ce87ac6aa3076107
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-16 17:38:38 +0300

    add passwordfield

--- app/models.py
+++ app/models.py
@@ -8,6 +8,8 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     cart_items = db.relationship('CartItem', backref='user', lazy=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
